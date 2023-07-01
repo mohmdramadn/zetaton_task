ba22e16de61b10eff86a12ec57a27d2d9c890bae
Mohamed Ramadan <mohmdramadn97@gmail.com>
2023-07-01 13:07:55 +0300

    Added register screen

---
+++ lib/screens/register/register_screen.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:zetaton_task/components/app_button.dart';
+import 'package:zetaton_task/constants/constant_strings.dart';
+import 'package:zetaton_task/contract/services/i_connection_service.dart';
+import 'package:zetaton_task/contract/services/i_firebase_service.dart';
+import 'package:zetaton_task/contract/services/i_message_service.dart';
+import 'package:zetaton_task/screens/register/register_view_model.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<RegisterViewModel>(
+        create: (_) => RegisterViewModel(
+              connectionService: context.read<IConnectionService>(),
+              messageService: context.read<IMessageService>(),
+              firebaseService: context.read<IFirebaseService>(),
+            ),
+        child: const _Body());
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const _Header(),
+              SizedBox(height: 4.0.h),
+              const _SubHeader(),
+              const _UserDetailsFields(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _UserDetailsFields extends StatelessWidget {
+  const _UserDetailsFields();
+
+  @override
+  Widget build(BuildContext context) {
+    var isEnabled =
+        context.select((RegisterViewModel vm) => vm.isButtonEnabled);
+    var isLoading = context.select((RegisterViewModel vm) => vm.isLoading);
+    var emailController =
+        context.select((RegisterViewModel vm) => vm.emailController);
+    var passwordController =
+        context.select((RegisterViewModel vm) => vm.passwordController);
+    var reEnterPasswordController =
+        context.select((RegisterViewModel vm) => vm.reEnterPassController);
+    var firstNameController =
+        context.select((RegisterViewModel vm) => vm.firstNameController);
+    var lastNameController =
+        context.select((RegisterViewModel vm) => vm.lastNameController);
+    var phoneController =
+        context.select((RegisterViewModel vm) => vm.phoneController);
+
+    var formKey =
+        context.select((RegisterViewModel vm) => vm.formKey);
+
+    return Form(
+      key: formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      onChanged: () => context
+          .read<RegisterViewModel>()
+          .setButtonEnabledState(formKey.currentState!.validate()),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _Field(
+            controller: firstNameController,
+            labelText: Constant.titles.firstName,
+            validator: (value) {
+              var validationState =
+              context.read<RegisterViewModel>().validateName(value!,true);
+              if (validationState != "") return validationState;
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          _Field(
+            controller: lastNameController,
+            labelText: Constant.titles.lastName,
+            validator: (value) {
+              var validationState =
+              context.read<RegisterViewModel>().validateName(value!,false);
+              if (validationState != "") return validationState;
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          _Field(
+            controller: emailController,
+            labelText: Constant.titles.email,
+            validator: (value) {
+              var validationState =
+                  context.read<RegisterViewModel>().validateEmail(value!);
+              if (validationState != "") return validationState;
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          _Field(
+            controller: passwordController,
+            labelText: Constant.titles.password,
+            obscureText: true,
+            validator: (value) {
+              var validationState =
+              context.read<RegisterViewModel>().validatePassword(value!);
+              if (validationState != "") return validationState;
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          _Field(
+            controller: reEnterPasswordController,
+            labelText: Constant.titles.reEnterPassword,
+            obscureText: true,
+            validator: (value) {
+              var validationState =
+              context.read<RegisterViewModel>().validateConfirmPassword();
+              if (validationState != "") return validationState;
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          _Field(
+            controller: phoneController,
+            labelText: Constant.titles.phoneNumber,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return Constant.titles.enterPhoneNumber;
+              }
+              // You can add additional validation logic here
+              return null;
+            },
+          ),
+          SizedBox(height: 16.0.h),
+          AppButton(
+            onPressed: !isEnabled
+                ? null
+                : () => context
+                    .read<RegisterViewModel>()
+                    .onRegisterSelectedAction(),
+            child: isLoading
+                ? SizedBox(
+                    height: 15.h,
+                    width: 15.w,
+                    child: const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  )
+                : Text(Constant.titles.register),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0, right: 8.0, left: 8.0),
+      child: Text(
+        Constant.titles.register,
+        style: TextStyle(
+          fontSize: 30.spMin,
+          color: Theme.of(context).primaryColorDark,
+        ),
+      ),
+    );
+  }
+}
+
+class _SubHeader extends StatelessWidget {
+  const _SubHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: Text(
+        Constant.titles.registerSubHeader,
+        style: TextStyle(
+          fontSize: 16.spMin,
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+  }
+}
+
+class _Field extends StatelessWidget {
+  const _Field({
+    required this.labelText,
+    required this.validator,
+    required this.controller,
+    this.obscureText = false,
+  });
+
+  final String labelText;
+  final String? Function(dynamic value) validator;
+  final bool obscureText;
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+        ),
+        obscureText: obscureText,
+        validator: validator,
+      ),
+    );
+  }
+}
---
+++ lib/screens/register/register_view_model.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:zetaton_task/constants/constant_strings.dart';
+import 'package:zetaton_task/contract/services/i_connection_service.dart';
+import 'package:zetaton_task/contract/services/i_firebase_service.dart';
+import 'package:zetaton_task/contract/services/i_message_service.dart';
+import 'package:zetaton_task/helper/validator.dart';
+
+class RegisterViewModel extends ChangeNotifier {
+  final IConnectionService connectionService;
+  final IFirebaseService firebaseService;
+  final IMessageService messageService;
+
+  RegisterViewModel({
+    required this.connectionService,
+    required this.firebaseService,
+    required this.messageService,
+  });
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController reEnterPassController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  void setLoadingState(value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
+  bool _isButtonEnabled = false;
+  bool get isButtonEnabled => _isButtonEnabled;
+  void setButtonEnabledState(value) {
+    _isButtonEnabled = value;
+    notifyListeners();
+  }
+
+  Future<void> onRegisterSelectedAction() async {
+    var isConnected = await connectionService.checkConnection();
+    if (!isConnected) {
+      Fluttertoast.showToast(
+        msg: Constant.titles.notConnected,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey,
+        textColor: Colors.white,
+        fontSize: 16.0.spMin,
+      );
+      return;
+    }
+    setLoadingState(true);
+    var signUpResponse = await firebaseService.signupNewUserAsync(
+        emailController.text, passwordController.text);
+    if (signUpResponse.isError) {
+      setLoadingState(false);
+      messageService.showErrorSnackBar(
+        '',
+        signUpResponse.asError!.error.toString(),
+      );
+      return;
+    }
+    setLoadingState(false);
+    notifyListeners();
+  }
+
+  String validatePhone(String phone) {
+    var phoneValidationResult = Validator.isValidPhone(phone);
+    if (phoneValidationResult == Constant.phoneValidationStatus.isEmpty) {
+      return Constant.phoneValidationStatus.isEmpty;
+    }
+    if (phoneValidationResult == Constant.phoneValidationStatus.isNotNumber) {
+      return Constant.phoneValidationStatus.isNotNumber;
+    }
+    return "";
+  }
+
+  String validateEmail(String email) {
+    var emailValidationResult = Validator.isValidEmail(email);
+    if (!emailValidationResult) {
+      return Constant.emailValidationStatus.isNotValid;
+    }
+    return "";
+  }
+
+  String validatePassword(String password) {
+    var passwordValidationResult = Validator.isValidPassword(password);
+    if (passwordValidationResult == Constant.passwordValidationStatus.isEmpty) {
+      return Constant.passwordValidationStatus.isEmpty;
+    }
+    return "";
+  }
+
+  String validateConfirmPassword() {
+    var passwordValidationResult = Validator.isValidConfirmPassword(
+      password: passwordController.text,
+      confirmPassword: reEnterPassController.text,
+    );
+    if (passwordValidationResult ==
+        Constant.reEnterPasswordValidationStatus.isEmpty) {
+      return Constant.reEnterPasswordValidationStatus.isEmpty;
+    }
+    if (passwordValidationResult ==
+        Constant.reEnterPasswordValidationStatus.isNotMatching) {
+      return Constant.reEnterPasswordValidationStatus.isNotMatching;
+    }
+    return "";
+  }
+
+  String validateName(String name, bool isFirstName) {
+    var nameValidationResult = Validator.isValidName(name);
+    if (!nameValidationResult) {
+      if (isFirstName) {
+        return Constant.titles.firstNameEmpty;
+      }
+      return Constant.titles.lastNameEmpty;
+    }
+    return "";
+  }
+}
