a24bad5e73471a65ab748fdc8c4c757fa11c3521
Mohamed Ramadan <mohmdramadn97@gmail.com>
2023-07-01 13:06:25 +0300

    Added validator

---
+++ lib/helper/validator.dart
@@ -0,0 +1,50 @@
+import 'package:zetaton_task/constants/constant_strings.dart';
+
+class Validator {
+  Validator._();
+
+  static String isValidPassword(String password) {
+    if (password.isEmpty) {
+      return Constant.passwordValidationStatus.isEmpty;
+    }
+    return Constant.passwordValidationStatus.isValid;
+  }
+
+  static String isValidConfirmPassword(
+      {required String password, required String confirmPassword}) {
+    if (confirmPassword.isEmpty) {
+      return Constant.reEnterPasswordValidationStatus.isEmpty;
+    } else if (!isMatchPassword(password, confirmPassword)) {
+      return Constant.reEnterPasswordValidationStatus.isNotMatching;
+    }
+    return Constant.reEnterPasswordValidationStatus.isValid;
+  }
+
+  static bool isValidEmail(String? email) {
+    const emailRegExpString = r'[a-zA-Z0-9\+\.\_\%\-\+]{1,256}\@[a-zA-Z0-9]'
+        r'[a-zA-Z0-9\-]{0,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{0,25})+';
+    return RegExp(emailRegExpString, caseSensitive: false).hasMatch(email!) ||
+        email.isEmpty;
+  }
+
+  static String isValidPhone(String? phone) {
+    String pattern =
+        r'^(?:\+?1[-.●]?)?\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$';
+
+    if (phone!.isEmpty) {
+      return Constant.phoneValidationStatus.isEmpty;
+    } else if (!RegExp(pattern).hasMatch(phone)) {
+      return Constant.phoneValidationStatus.isNotNumber;
+    }
+
+    return Constant.phoneValidationStatus.isValid;
+  }
+
+  static bool isValidName(String? userName) {
+    return userName != '';
+  }
+
+  static bool isMatchPassword(String password, String matchPassword) {
+    return password == matchPassword;
+  }
+}
