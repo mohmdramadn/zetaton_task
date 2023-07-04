c95a149df66e45edd3c940382a3da325b904188c
Mohamed Ramadan <mohmdramadn97@gmail.com>
2023-07-04 07:05:06 +0300

    Added create profile functions and add favorites function

--- lib/contract/services/i_firebase_service.dart
+++ lib/contract/services/i_firebase_service.dart
@@ -2,9 +2,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:async/async.dart';
 
 abstract class IFirebaseService {
-  Future<Result<User>> signupNewUserAsync(String email, String password);
+  Future<Result<User>> signupNewUserAsync(String email, String password,String fullName);
 
   Future<Result<User>> loginAsync(String email, String password);
+
+  Future<Result<String>> createProfileAsync({required String fullName});
+
+  Future<Result<bool>> addNewUserNameAsync({String? displayName});
+
+  Future<Result<bool>> updateFavoritesAsync({required List<String> photos});
+
+  Future<void> updateUserDisplayName(String fullName);
 
   Future<Result<bool>> logoutAsync();
 
--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:async/async.dart';
 import 'package:injectable/injectable.dart';
@@ -7,16 +8,19 @@ import 'package:zetaton_task/contract/services/i_firebase_service.dart';
 @Singleton(as: IFirebaseService)
 class FirebaseService implements IFirebaseService{
   final firebaseAuth = FirebaseAuth.instance;
+  final database = FirebaseFirestore.instance;
   User? user;
 
   @override
-  Future<Result<User>> signupNewUserAsync(String email, String password) async {
+  Future<Result<User>> signupNewUserAsync(String email, String password,
+      String fullName) async {
     try {
       var response = await firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
       user = response.user;
+      createProfileAsync(fullName: fullName);
       return Result.value(response.user!);
     } on FirebaseAuthException catch (e) {
       return Result.error(e.message!);
@@ -51,45 +55,45 @@ class FirebaseService implements IFirebaseService{
     }
   }
 
-  // Future<Result<String>> createProfileAsync(
-  //     {required UserProfileModel model}) async {
-  //   final newUser = <String, dynamic>{
-  //     'uid': user?.uid,
-  //     'Email': user?.email,
-  //     'Full name': model.fullName,
-  //     'username': model.username,
-  //     'Gender': model.gender,
-  //     'Height': model.height,
-  //     'Age': model.age,
-  //     'Muscles Percentage': model.musclesPercentage,
-  //     'Water Percentage': model.waterPercentage,
-  //     'Fats Percentage': model.fatsPercentage,
-  //     'Join Date': model.joinDate,
-  //     'Next session': model.nextSession
-  //   };
-  //   try {
-  //     await database.collection('users_profile').doc(user?.uid).set(newUser);
-  //     await addNewUserNameAsync(displayName: model.fullName);
-  //     return Result.value(user!.uid);
-  //   } catch (e) {
-  //     return Result.error(e);
-  //   }
-  // }
+  @override
+  Future<Result<bool>> addNewUserNameAsync({String? displayName}) async {
+    try {
+      await user?.updateDisplayName(displayName);
+      return Result.value(true);
+    } catch (e) {
+      return Result.error(e);
+    }
+  }
 
-  // Future<Result<UserProfileModel>> getUserProfile() async {
-  //   try {
-  //     var profile = await database
-  //         .collection('users_profile')
-  //         .where('uid', isEqualTo: user?.uid)
-  //         .get();
-  //     UserProfileModel userProfile =
-  //     UserProfileModel.fromFirestore(profile.docs.first.data());
-  //
-  //     return Result.value(userProfile);
-  //   } catch (e) {
-  //     return Result.error(e);
-  //   }
-  // }
+  @override
+  Future<Result<String>> createProfileAsync({required String fullName}) async {
+    final newUser = <String, dynamic>{
+      'uid': user?.uid,
+      'Full name': fullName,
+      'Favorites': []
+    };
+    try {
+      await database.collection('users').doc(user?.uid).set(newUser);
+      await addNewUserNameAsync(displayName: fullName);
+      return Result.value(user!.uid);
+    } catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  @override
+  Future<Result<bool>> updateFavoritesAsync(
+      {required List<String> photos}) async {
+    try {
+      await database
+          .collection('users')
+          .doc(user?.uid)
+          .update({'Favorites': FieldValue.arrayUnion(photos)});
+      return Result.value(true);
+    } catch (e) {
+      return Result.error(e);
+    }
+  }
 
   @override
   User? getCurrentUserInfo() {
@@ -102,5 +106,10 @@ class FirebaseService implements IFirebaseService{
       return true;
     }
     return false;
+  }
+
+  @override
+  Future<void> updateUserDisplayName(String fullName) async {
+    await firebaseAuth.currentUser?.updateDisplayName(fullName);
   }
 }
